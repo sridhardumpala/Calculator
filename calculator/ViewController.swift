651a51ffce875a70c2c2ce0359dc0e171a870e2f
Sridhar Dumpala <sridhardumpalak@gmail.com>
2017-07-11 13:25:13 -0700

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Sridhar Dumpala on 7/11/17.
+//  Copyright © 2017 Sridhar Dumpala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
